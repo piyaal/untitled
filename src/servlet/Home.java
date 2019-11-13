c3a1021b8cdf09ba00f1d5f269c22f3e32140d9c
Piyal <itpial48@gmail.com>
2019-11-14 02:56:25 +0600

    another commit

---
+++ src/servlet/Home.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Piyal on 11/13/2019.
+ */
+public class Home extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("f****** home visite");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>hello b***h</h1>");
+    }
+}
