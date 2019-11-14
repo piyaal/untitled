794db553b270851ab2c68b21f28285f1ee059a04
Piyal <itpial48@gmail.com>
2019-11-15 00:34:22 +0600

    bbbb

--- src/servlet/Home.java
+++ src/servlet/Home.java
@@ -14,11 +14,12 @@ public class Home extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("f****** home visite");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<h1>hello b***h</h1>");
+        out.println("<h1>hello b***h</h1>");
+        out.println("<h2>hello b***h</h2>");
     }
 }
